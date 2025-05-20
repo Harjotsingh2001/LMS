7e9f578dfe0da59b972ab18423fd282bee0ee6fa
Harjot Singh <singhjot4185@gmail.com>
2025-05-20 22:58:36 +0200

    start project LMS

---
+++ controller/Main.java
@@ -0,0 +1,12 @@
+package LMS.controller;
+
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame Frame = new JFrame();
+        Frame.setSize(500,500);
+        Frame.setVisible(true);
+    }
+    
+}
